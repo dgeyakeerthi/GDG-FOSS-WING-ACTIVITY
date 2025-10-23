d37cf8aae0a95ef980e7cf5232d3119103d37eb9
dgeyakeerthi <dgeyakeerthi@gmail.com>
2025-10-23 19:52:06 +0530

    JS

---
+++ script.js
@@ -0,0 +1,8 @@
+window.onload = function() {
+    alert("Welcome Geya Keerthi! 😊");
+};
+
+function showMessage() {
+    document.querySelector("p").textContent = "You clicked the button! Have a great day!";
+    document.querySelector("p").style.color = "darkgreen";
+}
